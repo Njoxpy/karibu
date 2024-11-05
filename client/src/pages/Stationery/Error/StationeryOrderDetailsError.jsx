2978a341acf9480e8b7525291311286177c28a95
Godbless Nyagawa <godblessnyagawa@gmail.com>
2024-11-05 13:36:00 +0300

    added stationery details error page for handling orders which are not found

---
+++ client/src/pages/Stationery/Error/StationeryOrderDetailsError.jsx
@@ -0,0 +1,11 @@
+function StationeryOrderDetailsError() {
+  return (
+    <>
+      <div className="p-4">
+        <h3>StationeryOrderDetailsError</h3>
+      </div>
+    </>
+  );
+}
+
+export default StationeryOrderDetailsError;
