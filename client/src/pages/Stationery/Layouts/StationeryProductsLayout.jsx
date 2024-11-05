3f9fff67df06ddccbb2bcef26c867183261194f7
Godbless Nyagawa <godblessnyagawa@gmail.com>
2024-11-05 13:37:21 +0300

    added stationery products layout and staionery manage page for admin

---
+++ client/src/pages/Stationery/Layouts/StationeryProductsLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+function StationeryProductsLayout() {
+  return (
+    <>
+      <div></div>
+      <Outlet />
+    </>
+  );
+}
+
+export default StationeryProductsLayout;
