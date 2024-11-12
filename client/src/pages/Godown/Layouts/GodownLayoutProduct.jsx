2e8449b6ce9debf7f99d577340153d71c5098f40
Godbless Nyagawa <godblessnyagawa@gmail.com>
2024-11-12 12:15:29 +0300

    adedd godown layout page

---
+++ client/src/pages/Godown/Layouts/GodownLayoutProduct.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+function GodownLayoutProduct() {
+  return (
+    <>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+export default GodownLayoutProduct;
