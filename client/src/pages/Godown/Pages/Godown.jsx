9676d766c943ce58622693928ce0219e52e80304
Godbless Nyagawa <godblessnyagawa@gmail.com>
2024-11-12 12:15:14 +0300

    removed useless code

--- client/src/pages/Godown/Pages/Godown.jsx
+++ client/src/pages/Godown/Pages/Godown.jsx
@@ -1,8 +1,6 @@
 import Footer from "../../../components/Footer";
-import SearchGodownItem from "../Components/SearchGownItem";
 import InventoryTable from "./InventoryTable";
 import OrdersTestings from "./OrderTesting";
-import TestingPagination from "./TestingPagination";
 
 const Godown = () => {
   return (
@@ -11,7 +9,6 @@ const Godown = () => {
         <h1 className="text-2xl font-bold mb-6 text-center">
           Godown Items Available
         </h1>
-        <SearchGodownItem />
         <InventoryTable />
 
         <OrdersTestings />
