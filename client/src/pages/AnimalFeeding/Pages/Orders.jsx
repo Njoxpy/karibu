77ca9e46ab3a9f8619c125318a7e26c89dec19b2
Godbless Nyagawa <110627172+Njoxpy@users.noreply.github.com>
2024-11-24 20:37:10 +0300

    test 3

--- client/src/pages/AnimalFeeding/Pages/Orders.jsx
+++ client/src/pages/AnimalFeeding/Pages/Orders.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Footer from "../../../components/Footer";
 import { Link } from "react-router-dom";
-import NoOrders from "../../../components/Noorders";
 
 const Orders = () => {
   // Local state for orders
@@ -69,7 +68,7 @@ const Orders = () => {
       <div className="p-4">
         <h1 className="text-2xl font-bold mb-4 text-center">Orders List</h1>
         {orders.length === 0 ? (
-          <NoOrders />
+          <p>no orders</p>
         ) : (
           <table className="min-w-full border border-gray-300">
             <thead>
