6694c0bbce90b14fe96bab3ac3441ff0073fb5ea
Godbless Nyagawa <110627172+Njoxpy@users.noreply.github.com>
2024-11-24 20:27:27 +0300

    modified

--- client/src/pages/Hardware/pages/HardwareOrders.jsx
+++ client/src/pages/Hardware/pages/HardwareOrders.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Footer from "../../../components/Footer";
 import { Link } from "react-router-dom";
-import NoOrders from "../../../components/Noorders";
 
 const HardwareOrders = () => {
   // Local state for orders
@@ -68,7 +67,7 @@ const HardwareOrders = () => {
       <div className="p-4">
         <h1 className="text-2xl font-bold mb-4 text-center">Orders List</h1>
         {orders.length === 0 ? (
-          <NoOrders />
+          <p>No orders for now</p>
         ) : (
           <table className="min-w-full border border-gray-300">
             <thead>
