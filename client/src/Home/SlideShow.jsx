7cbf3d19dbd6f1be078a3078004e5695eff90a04
Godbless Nyagawa <110627172+Njoxpy@users.noreply.github.com>
2024-11-26 12:18:35 +0300

    added animal feeding image and slideshow

--- client/src/Home/SlideShow.jsx
+++ client/src/Home/SlideShow.jsx
@@ -7,11 +7,13 @@ import "swiper/css/pagination";
 import stationery from "../assets/images/stationery1.jpg";
 import printing from "../assets/images/printing.jpg";
 import godown from "../assets/images/godown-new-11.jpg";
+import animalFeeding from "../assets/images/animalFeeding.jpg"
 
 const images = [
     { id: 1, src: stationery, alt: "Stationery products on display" },
     { id: 2, src: printing, alt: "Printing services in action" },
     { id: 3, src: godown, alt: "Well-organized warehouse" },
+    { id: 3, src: animalFeeding, alt: "animal feeding" },
 ];
 
 const HelloSection = () => {
@@ -19,8 +21,8 @@ const HelloSection = () => {
         <div className="relative bg-gray-100">
             {/* Hero Section */}
             <div className="py-16 text-center bg-indigo-600 text-white">
-                <h1 className="text-4xl font-bold">Transforming Ideas into Stunning Visual Designs</h1>
-                <p className="mt-4 text-lg">Let our experienced team bring your vision to life with tailored graphic design solutions.</p>
+                <h1 className="text-4xl font-bold">Greetings from Your Leading Provider of Quality Products</h1>
+                <p className="mt-4 text-lg">Leading provider of quality services and products across industries including Hardware, Fresh Oil, Stationery, and more..</p>
             </div>
 
             {/* Slideshow Section */}
