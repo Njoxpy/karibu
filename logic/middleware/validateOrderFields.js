0ff374dbf248b7d1c250f8257fbc14d27bc02687
Godbless Nyagawa <110627172+Njoxpy@users.noreply.github.com>
2024-11-26 15:45:00 +0300

    added for validating create order page

---
+++ logic/middleware/validateOrderFields.js
@@ -0,0 +1,19 @@
+const validateOrderFields = () => {
+    const { totalPrice, orderId, userId, productName, quantity, status, category } = req.body
+
+    if (totalPrice == null || !userId || !productName || quantity == null) {
+        return res.status(400).json({ message: "all fields are required" })
+    }
+
+    // validate price
+    if (typeof totalPrice !== "number" || totalPrice < 0) {
+        return res.status(400).json("Price must be a positive number")
+    }
+
+    // validate qouantity
+    if (typeof quantity !== "number" || quantity < 0) {
+        return res.status(400).json("Quantity must be a none negative number")
+    }
+}
+
+module.exports = validateOrderFields;
