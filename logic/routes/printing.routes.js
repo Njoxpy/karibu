87e5799df8b6dc12121809632225af6d53765ec8
Godbless Nyagawa <110627172+Njoxpy@users.noreply.github.com>
2024-11-27 18:46:25 +0300

    added printing page routes and testing create pdf request test

--- logic/routes/printing.routes.js
+++ logic/routes/printing.routes.js
@@ -2,11 +2,20 @@ const express = require("express")
 const { createSubmission } = require("../controllers/printing.controllers")
 const validatePrintingSubmission = require("../middleware/validatePrintingSubmission")
 const router = express.Router()
+const pdf = require("html-pdf")
 
 router.get("/", (req, res) => {
     res.json({ message: "get all printing orders from savarahha printing page" })
 })
 
 router.post("/", validatePrintingSubmission, createSubmission)
+router.post("/create-pdf", (req, res) => {
+    pdf.create("Njox", {}).toFile('me.pdf', (err) => {
+        if (err) {
+            res.json({ message: err })
+        }
+        return Promise.resolve()
+    })
+})
 
 module.exports = router
