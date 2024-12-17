7d4cba19f4459cab9716c87866cc6098e8edaa52
Godbless Nyagawa <godblessnyagawa@gmail.com>
2024-12-17 19:19:15 +0300

    added godown routes

--- logic/routes/godown.routes.js
+++ logic/routes/godown.routes.js
@@ -1,25 +1,38 @@
 const express = require("express")
 const router = express.Router()
 
+const { createGodownProduct, createGodownOrder, getAllGodownProducts, getAllGodownOrders, getAllGodownProductById, getAllGodownOrderById, updateGodownProductById, updateGodownOrderById, deleteGodownProduct, deleteGodownOrder } = require("../controllers/godown.controller")
+
+// create product
+router.post("/products", createGodownProduct)
+
+// bulk upload
+
+// create order
+router.post("/orders", createGodownOrder)
+
 // get all products
-router.get("/products", (req, res) => {
-    res.json({ message: "get all products" })
-})
-
-// get products by id
-
-// delete product by id
+router.get("/products", getAllGodownProducts)
 
 // get all orders
+router.get("/orders", getAllGodownOrders)
+
+// get product by id
+router.get("/products/:id", getAllGodownProductById)
 
 // get order by id
+router.get("/orders/:id", getAllGodownOrderById)
 
-// move item
+// update product by id
+router.patch("/products/:id", updateGodownProductById)
 
-/*
-before moving item
-- check if the item is available.
-- check quantity.
-*/
+// update order by id
+router.patch("/orders/:id", updateGodownOrderById)
+
+// delete product by id
+router.delete("/products/:id", deleteGodownProduct)
+
+// delete product by id
+router.delete("/orders/:id", deleteGodownOrder)
 
 module.exports = router
