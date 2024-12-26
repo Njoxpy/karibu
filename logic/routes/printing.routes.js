bfc223dbfd79053f9ac769f85b0b05ae0b9bb281
Godbless Nyagawa <godblessnyagawa@gmail.com>
2024-12-26 16:00:32 +0300

    fixe errors

--- logic/routes/printing.routes.js
+++ logic/routes/printing.routes.js
@@ -1,32 +1,29 @@
 const express = require("express")
 
 // controller
-const { createSubmission, getPrintingSubmission, getPrintingOrders, getSinglePrintingSubmission, getSinglePrintingOrder, updatePrintingOrder, deletePrintingOrder, updateOrderStatus, assignDesigner } = require("../controllers/printing.controller")
+const { createOrder, getPrintingOrders, getSinglePrintingOrder, updatePrintingOrder, deletePrintingOrder, updateOrderStatus } = require("../controllers/printing.controller")
 
-const validatePrintingSubmission = require("../middleware/validatePrintingSubmission")
-
+// router
 const router = express.Router()
 
 // middleware
 const validateObjectId = require("../middleware/validateObjectId")
+const validatePrintingOrder = require("../middleware/printing/validatePrintingOrder")
 
 // create
-router.post("/orders", createSubmission)
+router.post("/orders", validatePrintingOrder, createOrder)
 
 // orders
 router.get("/orders", getPrintingOrders)
 
 // order
-router.get("/orders/:id", getSinglePrintingOrder)
+router.get("/orders/:id", validateObjectId, getSinglePrintingOrder)
 
 // update
 router.patch("/orders/:id", validateObjectId, updatePrintingOrder)
 
 // delete
 router.delete("/orders/:id", validateObjectId, deletePrintingOrder)
-
-// assign designer
-router.put("orders/:id/assign-designer", assignDesigner)
 
 // update order status
 router.put("orders/:id/status", updateOrderStatus)
