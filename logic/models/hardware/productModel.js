dbe6d86a4a3ab8698b6d6bca2883d68c42fa00ad
Godbless Nyagawa <godblessnyagawa@gmail.com>
2025-01-13 19:21:59 +0300

    feat: enhance file upload middleware to restrict file types and update hardware product model to include image field

--- logic/models/hardware/productModel.js
+++ logic/models/hardware/productModel.js
@@ -1,44 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const hardwareProductSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: [true, "Product name is required"],
-        },
-        price: {
-            type: Number,
-            required: [true, "Price is required"],
-            min: [0, "Price must be at least 0"]
-        },
-        quantity: {
-            type: Number,
-            required: [true, "Quantity is required"],
-            min: [0, "Quantity must be at least 0"]
-        },
-        description: {
-            type: String,
-            required: [true, "Description is required"],
-            maxLength: [500, "Description is too long"]
-        },
-        totalPrice: {
-            type: Number,
-            min: [0, "Total price must be at least 0"]
-        },
-        userId: {
-            type: Schema.Types.ObjectId,
-            ref: "User",
-            required: [true, "Admin user is required to create a product"]
-        },
+  {
+    name: {
+      type: String,
+      required: [true, "Product name is required"],
     },
-    { timestamps: true }
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: [0, "Price must be at least 0"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Quantity is required"],
+      min: [0, "Quantity must be at least 0"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      maxLength: [500, "Description is too long"],
+    },
+    totalPrice: {
+      type: Number,
+      min: [0, "Total price must be at least 0"],
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Admin user is required to create a product"],
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
 );
 
 hardwareProductSchema.pre("save", function (next) {
-    this.totalPrice = this.quantity * this.price;
-    next();
+  this.totalPrice = this.quantity * this.price;
+  next();
 });
 
-const HardwareProduct = mongoose.model("HardwareProduct", hardwareProductSchema);
+const HardwareProduct = mongoose.model(
+  "HardwareProduct",
+  hardwareProductSchema
+);
 module.exports = HardwareProduct;
