da54a5e3016a872959648003584f4f23ec958340
Godbless Nyagawa <godblessnyagawa@gmail.com>
2025-01-17 16:44:16 +0300

    feat: enable authentication and category check for animal feeding report generation

--- logic/routes/animalFeeding.routes.js
+++ logic/routes/animalFeeding.routes.js
@@ -243,8 +243,8 @@ router.get(
 // Generate Animal Feeding Report
 router.get(
   "/reports",
-  // authenticate,
-  // checkCategory(["admin"]),
+  authenticate,
+  checkCategory(["admin"]),
   async (req, res) => {
     try {
       const { startDate, endDate } = req.query;
