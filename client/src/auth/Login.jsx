b3e01a51091073acd15f5d08385eb8cbfc87c403
njox <godblessnyagawa@gmail.com>
2025-01-22 13:08:57 -0800

    changed to log login

--- client/src/auth/Login.jsx
+++ client/src/auth/Login.jsx
@@ -153,7 +153,7 @@ const Login = () => {
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-indigo-600"
                 disabled={isLoading}
               >
-                {isLoading ? "Loading..." : "Sign in"}
+                {isLoading ? "Loading in..." : "Sign in"}
               </button>
             </div>
           </form>
