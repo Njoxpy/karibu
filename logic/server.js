406b67e9ba45885722d8ce621e84934eb53c851a
Njox Njox <godblessnyagawa@gmail.com>
2025-02-26 17:20:29 +0300

    removed rate request for a while

--- logic/server.js
+++ logic/server.js
@@ -108,7 +108,7 @@ const limiter = rateLimit({
   message: "Too many requests, please try again later.",
 });
 
-app.use(limiter);
+// app.use(limiter);
 
 // Routes
 app.use("/api/v1/animal-feeding", animalFeedingRoutes);
